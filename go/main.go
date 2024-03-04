6fcf25ec4f897869b3c2947a7ea5a145a291f53f
nhannamsiu <dangduynam95@gmail.com>
2024-03-04 11:00:49 +0700

    feat: add methods to modify expertise inside person, add notes to wrapper methods

--- go/main.go
+++ go/main.go
@@ -1,5 +1,6 @@
 package main
 
+import "fmt"
 /*
 #cgo LDFLAGS: -L./../target/debug -lrustffi
 #include "./../target/rustffi.h"
@@ -7,9 +8,21 @@ package main
 import "C"
 
 func main() {
+    fmt.Println("Create expertise and assign to person--------------------------")
     expertise := C.expertise_create(C.CString("web3"), 15)
     person := C.person_create(C.CString("nam"), expertise)
     C.person_print(person)
+
+    fmt.Println("Update expertise fields--------------------------")
+    C.expertise_update(expertise, C.CString("DeFi"), 17)
+    C.person_print(person)
+
+    fmt.Println("Destroy person--------------------------")
     C.person_destroy(person)
-//     C.person_print(person)
+    C.person_print(person)
+    C.expertise_print(expertise)
+
+    fmt.Println("Destroy expertise--------------------------")
+    C.expertise_destroy(expertise)
+    C.expertise_print(expertise)
 }
--- src/c_wrapper.rs
+++ src/c_wrapper.rs
@@ -33,6 +33,22 @@ pub unsafe extern "C" fn expertise_print(wrapper: *mut c_expertise) {
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn expertise_update(
+    wrapper: *mut c_expertise,
+    field: *const c_char,
+    experience: usize,
+) {
+    // raw pointer is unsafe, we always need to check if it valid before deref to avoid crash
+    // not that you cannot move ownership back to Rust because it would cause unsafe behaviors on both C and Rust
+    // luckly rust prevents that so this line won't work: let expertise = (*wrapper).expertise;
+    if !wrapper.is_null() {
+        let rust_field = CStr::from_ptr(field).to_str().expect("Bad encoding").to_owned();
+        (*wrapper).expertise.field = rust_field;
+        (*wrapper).expertise.experience = experience;
+    }
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn expertise_destroy(wrapper: *mut c_expertise) {
     if wrapper.is_null() {
         return;
@@ -65,8 +81,13 @@ pub unsafe extern "C" fn person_create(
 
 #[no_mangle]
 pub unsafe extern "C" fn person_print(wrapper: *mut c_person) {
-    println!("{:?}", (*wrapper).person);
-    println!("{:?}", *(*wrapper).person.expertise);
+    // raw pointer is unsafe, we always need to check if it valid before deref to avoid crash
+    if !wrapper.is_null() {
+        println!("{:?}", (*wrapper).person);
+    }
+    if !(*wrapper).person.expertise.is_null() {
+        println!("{:?}", *(*wrapper).person.expertise);
+    }
 }
 
 #[no_mangle]
